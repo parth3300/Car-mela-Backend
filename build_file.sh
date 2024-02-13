ccc3e2d42cf3c43f709206f7aa4307b2baa8223b
parth3300 <1111singalparth@gmail.com>
2024-02-13 14:49:51 +0530

    commit 4

--- build_file.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "BUILD START"
-python3.11 -m pip install -r requirements.txt
-python3.11 manage.py collectstatic --noinput --clear
-echo "BUILD END"
---
+++ build_files.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "BUILD START"
+pip install -r requirements.txt
+python manage.py collectstatic --noinput --clear
+echo "BUILD END"
