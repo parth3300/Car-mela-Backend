2820d453fb54efaa587ee2ce0e1b1a3facb2d565
parth3300 <1111singalparth@gmail.com>
2024-02-13 14:40:50 +0530

    second commit

--- build_file.sh
+++ build_file.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+
 echo "BUILD START"
-python 3.11 -m pip install -r requirements.txt
-python 3.11 manage.py collectstaic --noinput --clear
+python3.11 -m pip install -r requirements.txt
+python3.11 manage.py collectstatic --noinput --clear
 echo "BUILD END"
